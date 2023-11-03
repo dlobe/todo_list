1e9f6368c2ca9bbe0de952647c0833023a1813d7
davidlobe <lobedaf@yahoo.com>
2023-11-03 17:37:21 -0400

    db issue

--- app/database.py
+++ app/database.py
@@ -4,12 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-#load_dotenv()
+load_dotenv()
 
-#DATABASE_URL = os.getenv( 'DATABASE_URL',
-#    'mysql+mysqlconnector://admin:myCloudTec10@cloudevtec.cutenzlzct6y.#us-east-1.rds.amazonaws.com:3306/cloudtecrecords')
-
-DATABASE_URL = "mysql+mysqlconnector://admin:myCloudTec10@cloudevtec.cutenzlzct6y.us-east-1.rds.amazonaws.com:3306/cloudtecrecords"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
